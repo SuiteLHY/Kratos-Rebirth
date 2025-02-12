93bbb7031d3c6d055919421a6975791a35f88521
Suite <a471486944@gmail.com>
2025-02-13 00:09:54 +0100

    add HLS Video Player
    1. 實現視頻拆分的HLS解決方案

---
+++ source/js/hls-player.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const players = document.querySelectorAll(".hls-player");
+  if (players.length > 0 && Hls.isSupported()) {
+    players.forEach(video => {
+      const hls = new Hls();
+      hls.loadSource(video.querySelector("source").src);
+      hls.attachMedia(video);
+    });
+  }
+});
